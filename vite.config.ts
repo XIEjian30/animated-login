3d7848966a0435d8307107ea36f8c7359a24eba2
xiejian30 <daaaxie@163.com>
2026-03-31 11:44:34 +0200

    fix: set base path correctly for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/animated-login/',   // 使用仓库名称（小写）
+  base: '/animated-login/',   // 仓库名必须小写 + 斜杠
 })
