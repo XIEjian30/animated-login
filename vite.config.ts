4c65c7c064912d73aaf803b1d46af05611d847ac
xiejian30 <daaaxie@163.com>
2026-03-31 11:56:08 +0200

    fix: correct base path to fix 404 assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/animated-login/',   // 仓库名必须小写 + 斜杠
+  base: '/animated-login/',   // 必须是仓库名小写 + 斜杠结尾
 })
