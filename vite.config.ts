b21c3f8f8a2373d167b88bdb55db7a400ba93a15
xiejian30 <daaaxie@163.com>
2026-03-31 11:38:52 +0200

    fix: change base to relative path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/animated-login/',        // GitHub Pages 子路径
+  base: './',     // 改成这个试试（相对路径，更稳）
 })
